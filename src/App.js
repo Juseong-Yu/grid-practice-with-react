7834b4db404487db416cd0be202830ca0eaef84d
Juseong-Yu <aaa4591@naver.com>
2023-06-28 13:41:23 +0900

    grid practice

--- src/App.js
+++ src/App.js
@@ -3,21 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="item color1">Item1</div>
+      <div className="item color2">Item2</div>
+      <div className="item color3">Item3</div>
+      <div className="item color4">Item4</div>
+      <div className="item color5">Item5</div>
+      <div className="item color6">Item6</div>
     </div>
   );
 }
